ad587f2d639ffcf0f3375439a591cb9bd7f51f16
yaza-putu <yaza-putu@users.noreply.github.com>
2022-12-01 09:15:43 +0000

    Fix styling

--- src/LaravelGoogleDriveStorageServiceProvider.php
+++ src/LaravelGoogleDriveStorageServiceProvider.php
@@ -3,11 +3,10 @@
 namespace Yaza\LaravelGoogleDriveStorage;
 
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Yaza\LaravelGoogleDriveStorage\Commands\LaravelGoogleDriveStorageCommand;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\ServiceProvider;
 
 class LaravelGoogleDriveStorageServiceProvider extends PackageServiceProvider
 {
@@ -34,10 +33,10 @@ class LaravelGoogleDriveStorageServiceProvider extends PackageServiceProvider
                     __DIR__.'/config/google-drive-storage.php', 'filesystems'
                 );
             }
-            Storage::extend('google', function($app, $config) {
+            Storage::extend('google', function ($app, $config) {
                 $options = [];
 
-                if (!empty($config['teamDriveId'] ?? null)) {
+                if (! empty($config['teamDriveId'] ?? null)) {
                     $options['teamDriveId'] = $config['teamDriveId'];
                 }
 
