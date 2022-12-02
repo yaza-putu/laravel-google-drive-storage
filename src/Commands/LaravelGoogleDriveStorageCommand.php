bd27376e3000eb3431b14c2a7cdc3019bbc81daa
yaza-putu <yaza-putu@users.noreply.github.com>
2022-12-02 02:22:30 +0000

    Fix styling

--- src/Commands/LaravelGoogleDriveStorageCommand.php
+++ src/Commands/LaravelGoogleDriveStorageCommand.php
@@ -14,26 +14,26 @@ class LaravelGoogleDriveStorageCommand extends Command
 
     public function handle(): int
     {
-        if ($this->argument("name") == "publish") {
+        if ($this->argument('name') == 'publish') {
             try {
-               $config = config('filesystems.disk');
-               array_push($config, [
-                   'google' => [
-                       'driver' => 'google',
-                       'clientId' => env('GOOGLE_DRIVE_CLIENT_ID'),
-                       'clientSecret' => env('GOOGLE_DRIVE_CLIENT_SECRET'),
-                       'refreshToken' => env('GOOGLE_DRIVE_REFRESH_TOKEN'),
-                       'folder' => env('GOOGLE_DRIVE_FOLDER_ID'), // without folder is root of drive or team drive
-                       //'teamDriveId' => env('GOOGLE_DRIVE_TEAM_DRIVE_ID'),
-                   ]
-               ]);
+                $config = config('filesystems.disk');
+                array_push($config, [
+                    'google' => [
+                        'driver' => 'google',
+                        'clientId' => env('GOOGLE_DRIVE_CLIENT_ID'),
+                        'clientSecret' => env('GOOGLE_DRIVE_CLIENT_SECRET'),
+                        'refreshToken' => env('GOOGLE_DRIVE_REFRESH_TOKEN'),
+                        'folder' => env('GOOGLE_DRIVE_FOLDER_ID'), // without folder is root of drive or team drive
+                        //'teamDriveId' => env('GOOGLE_DRIVE_TEAM_DRIVE_ID'),
+                    ],
+                ]);
 
                 return self::SUCCESS;
             } catch (\Exception $exception) {
                 return self::FAILURE;
             }
         } else {
-            $this->comment("Mungkin yg dimagsud adalah `publish`");
+            $this->comment('Mungkin yg dimagsud adalah `publish`');
         }
     }
 }
